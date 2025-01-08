5939633f0426069dbd39796a39ec032cd0205ec7
Mahfuz <mahfughasanbd@gmail.com>
2025-01-08 20:57:17 +0600

    currect the error

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,35 +1,34 @@
 import React, { useEffect, useState } from "react";
-// import { Link } from "react-router";
 
 const Favorites = () => {
     const [favorites, setFavorites] = useState([]);
 
     useEffect(() => {
         const localData = localStorage.getItem("favoriteList");
-        const data = JSON.parse(localData);
+        const data = localData ? JSON.parse(localData) : []; // Provide a default empty array if localData is null
         setFavorites(data);
     }, []);
-
-    // console.log(favorites);
 
     return (
         <div className="favorites m-3">
             <h2 className="text-3xl font-bold mb-10 text-center">
-                Total Favorites items: {favorites.length}
+                Favorites items: {favorites.length}
             </h2>
             {favorites.length > 0 ? (
-                <div className="favorite-cards flex flex-wrap justify-center gap-10 ">
+                <div className="favorite-cards flex flex-wrap justify-center gap-10">
                     {favorites.map((item, index) => (
                         <div key={index} className="card">
                             <div className="card-detals card bg-base-100 w-64 shadow-2xl">
-                                <img src={item.url} alt="image" />
-                                <h3 className="text-2xl font-bold p-5">{item.name}</h3>
+                                <img src={item.url} alt={item.name} />
+                                <h3 className="text-xl font-bold p-5">
+                                    Name: {item.name}
+                                </h3>
                             </div>
                         </div>
                     ))}
                 </div>
             ) : (
-                <p>No favorites yet.</p>
+                <p className="text-center text-3xl shadow-xl p-8 mt-20 text-red-600 font-bold">No favorites yet.</p>
             )}
         </div>
     );
